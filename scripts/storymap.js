bd63f485b2aff557f60a750e2eb5455bf59a3796
mcbba <77134855+mcbba@users.noreply.github.com>
2021-03-15 12:01:14 -0700

    Update storymap.js

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -469,21 +469,18 @@ $(window).on('load', function() {
    */
   function changeAttribution() {
     var attributionHTML = $('.leaflet-control-attribution')[0].innerHTML;
-    var credit = 'View <a href="'
-      // Show Google Sheet URL if the variable exists and is not empty, otherwise link to Chapters.csv
-      + (typeof googleDocURL !== 'undefined' && googleDocURL ? googleDocURL : './csv/Chapters.csv')
-      + '" target="_blank">data</a>';
+    var credit = ' ';
 
     var name = getSetting('_authorName');
     var url = getSetting('_authorURL');
 
     if (name && url) {
       if (url.indexOf('@') > 0) { url = 'mailto:' + url; }
-      ' by <a href="' + url + '">' + name + '</a> | ';
+      credit += ' by <a href="' + url + '">' + name + '</a> | ';
     } else if (name) {
-      ' by ' + name + ' | ';
+      credit += ' by ' + name + ' | ';
     } else {
-      ' | ';
+      credit += ' | ';
     }
 
     credit += 'View <a href="' + getSetting('_githubRepo') + '">code</a>';
